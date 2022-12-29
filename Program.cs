b2c7ac15964e2b68f7f8f25ad623b12e5e2708b6
Ziad Mahmoud <116916124+Ziadma7moud@users.noreply.github.com>
2022-12-29 21:42:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discreet_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n1: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter n2: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            // Find and print the perfect numbers from n1 to n2
+            Console.WriteLine("Perfect numbers from " + n1 + " to " + n2 + ": ");
+            for (int i = n1; i <= n2; i++)
+            {
+                if (IsPerfectNumber(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static bool IsPerfectNumber(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum == n;
+        }
+    }
+    }
+
